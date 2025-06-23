279dbb11fbcf507c680cbf5637c818451a198d0e
ilafedoseev <devil.ilya@gmail.com>
2025-06-23 23:19:04 +0300

    check leading

--- lib/main.dart
+++ lib/main.dart
@@ -95,6 +95,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        leading: Icon(Icons.notification_add, color: Colors.amber),
       ),
       body: Container(
         padding: EdgeInsets.all(50.0),
