11a7011be0f4612c00aa17907e155570f60013d3
ilafedoseev <devil.ilya@gmail.com>
2025-07-05 11:20:25 +0300

    edit login screen

--- lib/views/pages/login_page.dart
+++ lib/views/pages/login_page.dart
@@ -1,7 +1,6 @@
+import 'package:fitness_crm/views/widget_tree.dart';
 import 'package:fitness_crm/views/widgets/hero_widget.dart';
 import 'package:flutter/material.dart';
-
-TextEditingController controller = TextEditingController();
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -11,9 +10,13 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController controllerEmail = TextEditingController();
+  TextEditingController controllerPassword = TextEditingController();
+
   @override
   void dispose() {
-    // TODO: implement dispose
+    controllerEmail.dispose();
+    controllerPassword.dispose();
     super.dispose();
   }
 
@@ -22,14 +25,45 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
-        padding: const EdgeInsets.all(10.0),
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
             HeroWidget(title: 'Login'),
+            SizedBox(height: 20.0),
             TextField(
-              controller: controller,
-              decoration: InputDecoration(border: OutlineInputBorder()),
+              controller: controllerEmail,
+              decoration: InputDecoration(
+                hintText: 'Email',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+              ),
               onEditingComplete: () => setState(() {}),
+            ),
+            SizedBox(height: 10.0),
+            TextField(
+              controller: controllerPassword,
+              decoration: InputDecoration(
+                hintText: 'Password',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+              ),
+              onEditingComplete: () => setState(() {}),
+            ),
+            SizedBox(height: 20.0),
+            FilledButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return WidgetTree();
+                    },
+                  ),
+                );
+              },
+              child: Text('Login'),
             ),
           ],
         ),
