2434f1755048bb1d54e237d4437478f03a620280
ilafedoseev <devil.ilya@gmail.com>
2025-07-09 20:29:28 +0300

    check LayoutBuilder

--- lib/views/pages/login_page.dart
+++ lib/views/pages/login_page.dart
@@ -32,47 +32,54 @@ class _LoginPageState extends State<LoginPage> {
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(20.0),
-            child: FractionallySizedBox(
-              widthFactor: 0.5,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Lottie.asset('assets/lotties/welcome.json', height: 400.0),
-                  SizedBox(height: 20.0),
-                  TextField(
-                    controller: controllerEmail,
-                    decoration: InputDecoration(
-                      hintText: 'Email',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15.0),
+            child: LayoutBuilder(
+              builder: (context, BoxConstraints contstrains) {
+                return FractionallySizedBox(
+                  widthFactor: contstrains.maxWidth > 640 ? 0.5 : 1.0,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Lottie.asset(
+                        'assets/lotties/welcome.json',
+                        height: 400.0,
                       ),
-                    ),
-                    onEditingComplete: () => setState(() {}),
-                  ),
-                  SizedBox(height: 10.0),
-                  TextField(
-                    controller: controllerPassword,
-                    decoration: InputDecoration(
-                      hintText: 'Password',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15.0),
+                      SizedBox(height: 20.0),
+                      TextField(
+                        controller: controllerEmail,
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                        ),
+                        onEditingComplete: () => setState(() {}),
                       ),
-                    ),
-                    onEditingComplete: () => setState(() {}),
+                      SizedBox(height: 10.0),
+                      TextField(
+                        controller: controllerPassword,
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                        ),
+                        onEditingComplete: () => setState(() {}),
+                      ),
+                      SizedBox(height: 20.0),
+                      FilledButton(
+                        style: FilledButton.styleFrom(
+                          minimumSize: Size(double.infinity, 55.0),
+                        ),
+                        onPressed: () {
+                          onLoginPressed();
+                        },
+                        child: Text(widget.title),
+                      ),
+                      SizedBox(height: 50.0),
+                    ],
                   ),
-                  SizedBox(height: 20.0),
-                  FilledButton(
-                    style: FilledButton.styleFrom(
-                      minimumSize: Size(double.infinity, 55.0),
-                    ),
-                    onPressed: () {
-                      onLoginPressed();
-                    },
-                    child: Text(widget.title),
-                  ),
-                  SizedBox(height: 50.0),
-                ],
-              ),
+                );
+              },
             ),
           ),
         ),
